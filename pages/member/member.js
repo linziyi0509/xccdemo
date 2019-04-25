2cbc8b8cd59bac4731c2ab70356622ec181407ac
dudj <13020078873@163.com>
2019-04-25 14:08:30 +0800

    获取地理位置

--- pages/member/member.js
+++ pages/member/member.js
@@ -38,16 +38,47 @@ Page({
             userInfo: res.userInfo,
             hasUserInfo: true
           })
+        },
+        error:res=>{
+          console.log('error'+res);
         }
       })
     }
   },
   getUserInfo: function (e) {
     console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+    if(e.detail.errMsg){
+      wx.showModal({
+        title: '警告',
+        content: '您点击了拒绝授权,将无法正常显示个人信息,点击确定重新获取授权。',
+        success: function (res) {
+          if (res.confirm) {
+            wx.openSetting({
+              success: (res) => {
+                if (res.authSetting["scope.userInfo"]) {////如果用户重新同意了授权登录
+                  wx.getUserInfo({
+                    success: function (res) {
+                      app.globalData.userInfo = res.userInfo
+                      this.setData({
+                        userInfo: res.userInfo,
+                        hasUserInfo: true
+                      })
+                    }
+                  })
+                }
+              }, fail: function (res) {
+
+              }
+            })
+          }
+        }
+      })
+    }else{
+      app.globalData.userInfo = e.detail.userInfo
+      this.setData({
+        userInfo: e.detail.userInfo,
+        hasUserInfo: true
+      })
+    }
   }
 })
--- pages/nearby/nearby.js
+++ pages/nearby/nearby.js
@@ -19,25 +19,93 @@ Page({
 
   },
   onLoad: function () {
-    this.getLocation();
+    this.getPermission(0, this);
   },
-  getLocation:function(){
+  //选择获取地理位置
+  getAddress: function () {
     var that = this;
-    wx.getLocation({
-      type: 'gcj02',
-      success: function (res) {
-        wx.chooseLocation({
-          type: 'wgs84',
-          success: res => {
-            console.log('location---:' + res.name + '--' + res.address + '---' + res.latitude + '---' + res.longitude);
-            that.setData({
-              empty: false,
-              latitude: res.latitude,
-              longitude: res.longitude
-            })
-          }
-        })
-      }
+    this.getPermission(1,that);
+  }, 
+  //获取用户地理位置权限
+  getPermission: function (once,obj) {
+    if (once == 0){
+      wx.chooseLocation({
+        success: function (res) {
+          console.log('chooseLocation---' + res);
+          console.log('location---:' + res.name + '--' + res.address + '---' + res.latitude + '---' + res.longitude);
+          obj.setData({
+            empty: false,
+            latitude: res.latitude,
+            longitude: res.longitude
+          })
+        }
     })
-  }
+    }else{
+      wx.chooseLocation({
+        success: function (res) {
+          console.log('chooseLocation---' + res);
+          console.log('location---:' + res.name + '--' + res.address + '---' + res.latitude + '---' + res.longitude);
+          obj.setData({
+            empty: false,
+            latitude: res.latitude,
+            longitude: res.longitude
+          })
+        },
+        fail: function (failres) {
+          console.log('failres:'+failres.errMsg);
+          wx.getSetting({
+            success: function (res) {
+              var statu = res.authSetting;
+              if (!statu['scope.userLocation']) {
+                wx.showModal({
+                  title: '是否授权当前位置',
+                  content: '需要获取您的地理位置，请确认授权，否则地图功能将无法使用',
+                  success: function (tip) {
+                    if (tip.confirm) {
+                      wx.openSetting({
+                        success: function (data) {
+                          if (data.authSetting["scope.userLocation"] === true) {
+                            wx.showToast({
+                              title: '授权成功',
+                              icon: 'success',
+                              duration: 1000
+                            })
+                            //授权成功之后，再调用chooseLocation选择地方
+                            wx.chooseLocation({
+                              success: function (res) {
+                                console.log('chooseLocation---' + res);
+                                console.log('location---:' + res.name + '--' + res.address + '---' + res.latitude + '---' + res.longitude);
+                                this.setData({
+                                  empty: false,
+                                  latitude: res.latitude,
+                                  longitude: res.longitude
+                                })
+                              },
+                            })
+                          } else {
+                            wx.showToast({
+                              title: '授权失败',
+                              icon: 'success',
+                              duration: 1000
+                            })
+                          }
+                        }
+                      })
+                    }
+                  }
+                })
+              }
+            },
+            fail: function (res) {
+              wx.showToast({
+                title: '调用授权窗口失败',
+                icon: 'success',
+                duration: 1000
+              })
+            }
+          })
+        }
+      })
+    }
+  },
 })
